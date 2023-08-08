6e807a69f8ec114dd55b1047b211641b5caa2aac
Ahmet Turan <1190505037@ogrenci.kirklareli.edu.tr>
2023-08-08 17:41:59 +0300

    08082023
    Bootcamp Day 2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _01_MVC_techbootcamp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
